8f69d00942be169f742d429434cb5f3b869ac9db
LegoStax <staxbusiness@gmail.com>
2016-04-11 19:05:14 -0700

    Updating stuffs

--- scripts/latestvid.js
+++ scripts/latestvid.js
@@ -6,14 +6,17 @@ function dummy(j){
 	$("#latestvid").attr("src", "https://www.youtube.com/embed/"+id);
 	$("#latestvid").show();
 };
-function getLatest(){
-	$("#latestvid").hide();
+function getLatest(videos){
 	$(document).ready(function(){
 		var uploadID = "UUTksBrSxoiEgL_iTLqkvHig";
 		var APIkey = "AIzaSyA8Nlwzno4COtvrlO1wqs5c9KtFoDRI1q8";
 		$.get("https://www.googleapis.com/youtube/v3/playlistItems?key="+APIkey+"&part=contentDetails&playlistId="+uploadID+"&maxResults=1", function(data, status){
-			if(status === "success"){
+			if(status === "success" && videos === true){
 				dummy(data);
+			}else if(status === "success" && videos === false){
+				console.log("got data");
+				return data;
+				//return data.items[0].contentDetails.videoId;
 			}
 		});
 	});
