99b730e061a12e699b7d4773051573fb37013ea7
legostax <legostax@gmail.com>
2016-05-12 11:12:25 -0700

    Update redirect.js

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,6 +1,8 @@
 // If HTTP then redirect to HTTPS
-var url = window.location.href;
-if(url.substring(0,5) !== "https"){
-  var newurl = "https"+url.substring(4);
-  window.location.href=newurl;
-}
+function checkHTTPS(){
+  var url = window.location.href;
+  if(url.substring(0,5) !== "https"){
+    var newurl = "https"+url.substring(4);
+    window.location.href=newurl;
+  }
+};
