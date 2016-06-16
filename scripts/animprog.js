7f5bf827eb436698189c733e38c2cde2a8d0f23e
LegoStax <staxbusiness@gmail.com>
2016-06-16 14:17:57 -0700

    Made progress update easier
    Added separate javascript file to more easily update animation progress.

---
+++ scripts/animprog.js
@@ -0,0 +1,9 @@
+function setParams(){
+	title = "How The Force Awakens Should Have Ended (rebuilding a new set and restarting animation)";
+	script = 100;
+	set_building = 100;
+	animation = 30;
+	compositing = 30;
+	effects = 10;
+	date = "June 15, 2016";
+}
