fd19377dbef21d919b31aa6f39c14f950c65bd49
LegoStax <staxbusiness@gmail.com>
2016-06-30 20:06:41 -0700

    Updated Animation Progress

--- scripts/animprog.js
+++ scripts/animprog.js
@@ -1,9 +1,9 @@
 function setParams(){
-	title = "How The Force Awakens Should Have Ended (rebuilding a new set and restarting animation)";
+	title = "How The Force Awakens Should Have Ended (uploading)";
 	script = 100;
-	set_building = 98;
-	animation = 98;
-	compositing = 96;
-	effects = 97;
-	date = "June 28, 2016";
+	set_building = 100;
+	animation = 100;
+	compositing = 100;
+	effects = 100;
+	date = "June 30, 2016";
 }
