3421124dee976277a1d91778639a163e28dc0593
legostax <staxbusiness@gmail.com>
2016-07-28 19:15:18 -0700

    Updated Animation Progress

--- scripts/animprog.js
+++ scripts/animprog.js
@@ -1,9 +1,9 @@
 function setParams(){
 	title = "Sight and Sound contest entry (untitled)";
 	script = 99;
-	set_building = 12; // total of 8 sets
-	animation = 0;
+	set_building = 14; // total of 8 sets
+	animation = 0.7; // total of 42 shots
 	compositing = 0;
 	effects = 0;
-	date = "July 21, 2016";
+	date = "July 28, 2016";
 }
