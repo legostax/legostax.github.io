3395fa9fb304d4b1f4986a77bbe23031b5a9f42a
LegoStax <legostax@gmail.com>
2016-07-30 20:40:22 -0700

    Oops

--- scripts/animprog.js
+++ scripts/animprog.js
@@ -2,7 +2,7 @@ function setParams(){
 	title = "Sight and Sound contest entry (untitled)";
 	script = 99;
 	set_building = 14; // total of 8 sets
-	animation = 4.7; // total of 42 shots
+	animation = 4; // total of 42 shots
 	compositing = 0;
 	effects = 0;
 	date = "July 30, 2016";
