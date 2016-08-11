6e5580ba1c85c979f6868c692892faa9d3cce76b
LegoStax <legostax@gmail.com>
2016-08-11 14:25:46 -0700

    Finishing UI overhaul

--- scripts/animprog.js
+++ scripts/animprog.js
@@ -2,7 +2,7 @@ function setParams(){
 	title = "Sight and Sound contest entry (untitled)";
 	script = 100;
 	set_building = 26; // total of 8 sets
-	animation = 20.9; // total of 43 shots
+	animation = 20.9; // 9/43 shots
 	compositing = 2;
 	effects = 0;
 	date = "August 5, 2016";
