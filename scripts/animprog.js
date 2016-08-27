24988066236032814ad19cc02b2263f9648d8520
LegoStax <legostax@gmail.com>
2016-08-26 21:48:35 -0700

    Updated Animation Progress

--- scripts/animprog.js
+++ scripts/animprog.js
@@ -2,8 +2,8 @@ function setParams(){
 	title = "Sight and Sound contest entry (untitled)";
 	script = 100;
 	set_building = 75; // 6/8 sets
-	animation = 41.9; // 18/43 shots
+	animation = 46.5; // 20/43 shots
 	compositing = 2;
 	effects = 0;
-	date = "August 23, 2016";
+	date = "August 26, 2016";
 }
