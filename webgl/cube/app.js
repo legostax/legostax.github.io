e94e49770efdbe12d4761f5238dc4aa5d9382051
Lego Stax <legostax@gmail.com>
2017-03-09 17:45:48 -0700

    Update app.js

--- webgl/cube/app.js
+++ webgl/cube/app.js
@@ -7,7 +7,7 @@ uniform mat4 mView;
 uniform mat4 mProj;
 void main(){
     fragColor = vertColor;
-    gl_Position = mProj * mView * mWorld * vec4(vertPosition, 0.0, 1.0);
+    gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);
 }
 `
 
@@ -34,6 +34,10 @@ var initDemo = function(){
 
     gl.clearColor(0.75, 0.85, 0.8, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+    gl.cullFace(gl.BACK);
+    gl.frontFace(gl.CCW);
 
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -67,14 +71,76 @@ var initDemo = function(){
 
     // Buffer
 
-    var triangleVertices = [ // each line is 1 point (X,Y), then RGB
-        0.0, 0.5, 0.0,    1.0, 1.0, 0.0,
-        -0.5, -0.5, 0.0,  0.7, 0.0, 1.0,
-        0.5, -0.5, 0.0,  0.1, 1.0, 0.6
+    var boxVertices = [ // each line is 1 point (X,Y), then RGB
+        // top
+        -1.0, 1.0, -1.0,   0.5, 0.5, 0.5,
+        -1.0, 1.0, 1.0,    0.5, 0.5, 0.5,
+        1.0, 1.0, 1.0,     0.5, 0.5, 0.5,
+        1.0, 1.0, -1.0,    0.5, 0.5, 0.5,
+
+        // left
+        -1.0, 1.0, 1.0,    0.75, 0.25, 0.5,
+        -1.0, -1.0, 1.0,   0.75, 0.25, 0.5,
+        -1.0, -1.0, -1.0,  0.75, 0.25, 0.5,
+        -1.0, 1.0, -1.0,   0.75, 0.25, 0.5,
+
+        // Right
+        1.0, 1.0, 1.0,     0.25, 0.25, 0.75,
+        1.0, -1.0, 1.0,    0.25, 0.25, 0.75,
+        1.0, -1.0, -1.0,   0.25, 0.25, 0.75,
+        1.0, 1.0, -1.0,    0.25, 0.25, 0.75,
+
+        // Front
+        1.0, 1.0, 1.0,     1.0, 0.0, 0.15,
+        1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
+        -1.0, -1.0, 1.0,   1.0, 0.0, 0.15,
+        -1.0, 1.0, 1.0,     1.0, 0.0, 0.15,
+
+        // Back
+        1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
+        1.0, -1.0, -1.0,   0.0, 1.0, 0.15,
+        -1.0, -1.0, -1.0,  0.0, 1.0, 0.15,
+        -1.0, 1.0, -1.0,   0.0, 1.0, 0.15,
+
+        // Bottom
+        -1.0, -1.0, -1.0,  0.5, 0.5, 1.0,
+        -1.0, -1.0, 1.0,   0.5, 0.5, 1.0,
+        1.0, -1.0, 1.0,    0.5, 0.5, 1.0,
+        1.0, -1.0, -1.0,   0.5, 0.5, 1.0
     ];
-    var triangleVertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
+
+    var boxIndices = [
+        // Top
+        0, 1, 2,
+        0, 2, 3,
+
+        // Left
+        5, 4, 6,
+        6, 4, 7,
+
+        // Right
+        8, 9, 10,
+        8, 10, 11,
+
+        // Front
+        13, 12, 14,
+        15, 14, 12,
+
+        // Back
+        16, 17, 18,
+        16, 18, 19,
+
+        // Bottom
+        21, 20, 22,
+        22, 20, 23
+    ];
+    var boxVertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW);
+
+    var boxIndexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
 
     var positionAttribLocation = gl.getAttribLocation(program, "vertPosition");
     var colorAttribLocation = gl.getAttribLocation(program, "vertColor");
@@ -83,22 +149,43 @@ var initDemo = function(){
 
     gl.enableVertexAttribArray(positionAttribLocation);
     gl.enableVertexAttribArray(colorAttribLocation);
-	
+
+    gl.useProgram(program);
+
 	var matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
 	var matViewUniformLocation = gl.getUniformLocation(program, 'mView');
 	var matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
-	
+
 	var worldMatrix = new Float32Array(16);
 	var viewMatrix = new Float32Array(16);
 	var projMatrix = new Float32Array(16);
 	mat4.identity(worldMatrix);
-	mat4.identity(viewMatrix);
-	mat4.identity(projMatrix);
-	
-	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE);
-	
-    // Main render loop
+	mat4.lookAt(viewMatrix, [0, 0, -8], [0, 0, 0], [0, 1, 0]);
+    mat4.perspective(projMatrix, glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000.0);
 
-    gl.useProgram(program);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+    gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
+    gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+
+    var xRotationMatrix = new Float32Array(16);
+    var yRotationMatrix = new Float32Array(16);
+
+
+    // Main render loop
+    var identityMatrix = new Float32Array(16);
+    mat4.identity(identityMatrix);
+    var angle = 0
+    var loop = function(){
+        angle = performance.now() / 1000 / 6 * 2 * Math.PI;
+        mat4.rotate(xRotationMatrix, identityMatrix, angle, [0, 1, 0]);
+        mat4.rotate(yRotationMatrix, identityMatrix, angle / 4, [1, 0, 0]);
+        mat4.mul(worldMatrix, xRotationMatrix, yRotationMatrix);
+        gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+        gl.clearColor(0.75, 0.85, 0.8, 1.0);
+        gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+        gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
+
+        requestAnimationFrame(loop);
+    };
+    requestAnimationFrame(loop);
 };
