d0a30d41b6ea75dce08a5b49271e72a17686397c
LegoStax <legostax@gmail.com>
2017-05-04 16:07:37 -0700

    Removed HTTPS check

--- scripts/redirect.js
+++
@@ -1,9 +0,0 @@
-// If HTTP then redirect to HTTPS
-function checkHTTPS(){
-  var url = window.location.href;
-  if(url.substring(0,5) !== "https"){
-    var newurl = "https"+url.substring(4);
-    window.location.href=newurl;
-  }
-  console.log("ran checkhttps");
-};
