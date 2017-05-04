c3664622f00ab872080127f5c3c439f962810970
LegoStax <legostax@gmail.com>
2017-05-04 15:49:18 -0700

    Updated Animation Progress

--- scripts/animprog.js
+++ scripts/animprog.js
@@ -1,9 +1,9 @@
 function setParams(){
 	title = "Obstipation (will resume Sight and Sound in summer 2017)";
 	script = 100;
-	set_building = 98; // 6/8 sets
-	animation = 0; // 21/43 shots
+	set_building = 100;
+	animation = 0;
 	compositing = 0;
 	effects = 0;
-	date = "April 23, 2017";
+	date = "May 4, 2017";
 }
