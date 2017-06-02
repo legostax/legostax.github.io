d25fc8ba9d14dc9132f5dcfcdadec2c50816b9d6
LegoStax <legostax@gmail.com>
2017-06-01 20:12:06 -0700

    Updated terminal and video system

--- scripts/latestvid.js
+++ scripts/latestvid.js
@@ -6,18 +6,15 @@ function dummy(j){
 	$("#latestvid").attr("src", "https://www.youtube.com/embed/"+id);
 	$("#latestvid").show();
 };
-function getLatest(videos){
+function getLatest(obj){
 	$(document).ready(function(){
 		var uploadID = "UUTksBrSxoiEgL_iTLqkvHig";
 		var APIkey = "AIzaSyA8Nlwzno4COtvrlO1wqs5c9KtFoDRI1q8";
 		$.get("https://www.googleapis.com/youtube/v3/playlistItems?key="+APIkey+"&part=contentDetails&playlistId="+uploadID+"&maxResults=1", function(data, status){
-			if(status === "success" && videos === true){
-				dummy(data);
-			}else if(status === "success" && videos === false){
-				console.log("got data");
-				console.log("data is: "+data.items[0].contentDetails.videoId);
-				return data;
-				//return data.items[0].contentDetails.videoId;
+			if(status === "success" && obj === undefined){
+				dummy(data); // To prevent Adblock from blocking the player
+			}else if(status === "success"){
+				obj.echo("Latest video: https://www.youtube.com/watch?v="+data.items[0].contentDetails.videoId);
 			}
 		});
 	});
