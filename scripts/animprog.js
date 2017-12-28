4596399255651992578c8ca38f991d4dbf726b94
Lego Stax <legostax@gmail.com>
2017-12-28 13:32:02 -0700

    Update animprog.js

--- scripts/animprog.js
+++ scripts/animprog.js
@@ -1,9 +1,9 @@
 function setParams(){
 	title = "Untitled Western Duel";
-	script = 50;
-	set_building = 10;
+	script = 100;
+	set_building = 100;
 	animation = 0;
 	compositing = 0;
 	effects = 0;
-	date = "December 27, 2017";
+	date = "December 28, 2017";
 }
